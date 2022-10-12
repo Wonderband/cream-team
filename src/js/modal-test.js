6a5e1fa5766faf793a3d9b6944576910cef41351
Alex Koniushenko <alex.koniushenko@gmail.com>
2022-10-12 14:31:51 +0300

    Update modal-test.js

--- src/js/modal-test.js
+++ src/js/modal-test.js
@@ -1,18 +1,3 @@
-// (() => {
-//     const refs = {
-//       openModalBtn: document.querySelector("[data-modal-test]"),
-//       closeModalBtn: document.querySelector("[data-modal-best]"),
-//       modal: document.querySelector("[data-modal-window]"),
-//     };
-  
-//     refs.openModalBtn.addEventListener("click", toggleModal);
-//     refs.closeModalBtn.addEventListener("click", toggleModal);
-  
-//     function toggleModal() {
-//       refs.modal.classList.toggle("is-hidden");
-//     }
-//   })();
-
   (() => { 
     document.addEventListener("click", (e)=> {      
       let currentModal = null;
